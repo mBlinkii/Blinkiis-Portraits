2af8424fcdeee888fba1aa9cbbc7ab77f4803f76
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-12-11 19:23:26 +0100

    🛠️ IMPROVE: mor work

--- Blinkiis_Portraits/core.lua
+++ Blinkiis_Portraits/core.lua
@@ -11,6 +11,61 @@ local addonName, _ = ...
 local defaults = {
 	test = true,
 	test_tbl = { a = "aaaa", b = "bbb" },
+	player = {
+		cast = false,
+		enable = true,
+		flip = false,
+		level = 20,
+		mirror = false,
+		point = { point = "RIGHT", relativePoint = "LEFT", x = 0, y = 0 },
+		size = 90,
+		strata = "AUTO",
+		texture = "drop",
+	},
+	target = {
+		cast = false,
+		enable = true,
+		flip = false,
+		level = 20,
+		mirror = false,
+		point = { point = "LEFT", relativePoint = "RIGHT", x = 0, y = 0 },
+		size = 90,
+		strata = "AUTO",
+		texture = "drop",
+	},
+	focus = {
+		cast = false,
+		enable = true,
+		flip = false,
+		level = 20,
+		mirror = false,
+		point = { point = "LEFT", relativePoint = "RIGHT", x = 0, y = 0 },
+		size = 90,
+		strata = "AUTO",
+		texture = "drop",
+	},
+	targettarget = {
+		cast = false,
+		enable = true,
+		flip = false,
+		level = 20,
+		mirror = false,
+		point = { point = "LEFT", relativePoint = "RIGHT", x = 0, y = 0 },
+		size = 90,
+		strata = "AUTO",
+		texture = "drop",
+	},
+	pet = {
+		cast = false,
+		enable = true,
+		flip = false,
+		level = 20,
+		mirror = false,
+		point = { point = "RIGHT", relativePoint = "LEFT", x = 0, y = 0 },
+		size = 90,
+		strata = "AUTO",
+		texture = "drop",
+	},
 }
 
 -- main function/ db loader
@@ -26,7 +81,7 @@ local function OnEvent(self, event, addOnName)
 
 		self:InitializeOptions()
 	elseif event == "PLAYER_LOGOUT" then
-		BlinkiisPortraitsDB = BLINKIISPORTRAITS.DB
+		BlinkiisPortraitsDB = BLINKIISPORTRAITS.db
 	else
 		BLINKIISPORTRAITS:Initialize()
 	end
@@ -179,7 +234,7 @@ local unitframesDB = {
 		party = "SUFHeaderpartyUnitButton",
 		boss = "SUFHeaderboss",
 		arena = "SUFHeaderArena",
-	},
+	},6
 }
 
 local function UpdatePortrait(frame, event, c, d, e, f)
@@ -189,79 +244,150 @@ local function UpdatePortrait(frame, event, c, d, e, f)
 		event == "UNIT_PORTRAIT_UPDATE"
 		or event == "PORTRAITS_UPDATED"
 		or (frame.typ == "target" and event == "PLAYER_TARGET_CHANGED")
+		or (frame.typ == "focus" and event == "PLAYER_FOCUS_CHANGED")
+		or ((frame.typ == "focus" or frame.typ == "targettarget") and event == "GROUP_ROSTER_UPDATE")
+		or (frame.typ == "targettarget" and event == "UNIT_TARGET")
 	then
 		BLINKIISPORTRAITS:Print(frame.unit)
 		SetPortraitTexture(frame.portrait, frame.unit, true)
 
-		if not InCombatLockdown() and frame:GetAttribute("unit") ~= frame.unit then frame:SetAttribute("unit", unit) end
+		if not InCombatLockdown() and frame:GetAttribute("unit") ~= frame.unit then
+			frame:SetAttribute("unit", unit)
+		end
+	end
+end
+
+local function UpdatePetPortrait(frame, event, c, d, e, f)
+	BLINKIISPORTRAITS:Print(frame, event, c, d, e, f)
+	BLINKIISPORTRAITS:Print("Typ", frame.typ, "Condition", (frame.typ == "target" and event == "PLAYER_TARGET_CHANGED"))
+	local unit = nil
+
+	if event == "UNIT_PET" or event == "UNIT_EXITED_VEHICLE" or event == "PET_UI_UPDATE" then
+		if UnitInVehicle("player") then
+			if UnitHasVehiclePlayerFrameUI("player") then
+				unit = "player"
+			end
+		else
+			unit = "pet"
+		end
+	end
+
+	frame.unit = unit or frame.unit
+
+	if
+		event == "UNIT_PORTRAIT_UPDATE"
+		or event == "PORTRAITS_UPDATED"
+		or event == "UNIT_PET"
+		or event == "UNIT_EXITED_VEHICLE"
+		or event == "PET_UI_UPDATE"
+	then
+		BLINKIISPORTRAITS:Print(unit or frame.unit)
+		SetPortraitTexture(frame.portrait, frame.unit, true)
+
+		if not InCombatLockdown() and frame:GetAttribute("unit") ~= frame.unit then
+			frame:SetAttribute("unit", unit)
+		end
 	end
 end
 
 -- portraits
 local function CreatePortrait(unitType, unitframes, events)
-	local unitFrame = _G[unitframes[unitType]]
-	local BPP = BLINKIISPORTRAITS.Portraits
+    local unitFrame = _G[unitframes[unitType]]
+    local BPP = BLINKIISPORTRAITS.Portraits
 
-	if not BPP[unitType] and unitFrame then
-		local portrait = CreateFrame("Button", "BP_Portrait_" .. unitType, unitFrame, "SecureUnitButtonTemplate")
-		portrait.parentFrame = unitFrame
-		portrait.unit = unitFrame.unit
-		portrait.typ = unitType
+    if not BPP[unitType] and unitFrame then
+        local portrait = CreateFrame("Button", "BP_Portrait_" .. unitType, unitFrame, "SecureUnitButtonTemplate")
 
-		for _, event in ipairs(events) do
-			portrait:RegisterEvent(event)
-		end
+        local settings = BLINKIISPORTRAITS.db[unitType]
+        portrait.Settings = settings
+        portrait.parentFrame = unitFrame
+        portrait.unit = unitFrame.unit
+        portrait.typ = unitType
+        portrait.size = settings.size
+        portrait.point = settings.point
+        portrait.textureFile = "Interface\\Addons\\Blinkiis_Portraits\\texture.tga"
+        portrait.bgFile = "Interface\\Addons\\Blinkiis_Portraits\\blank.tga"
+        portrait.maskFile = "Interface\\Addons\\Blinkiis_Portraits\\mask.tga"
+        portrait.rareFile = "Interface\\Addons\\Blinkiis_Portraits\\texture.tga"
+        portrait.eliteFile = "Interface\\Addons\\Blinkiis_Portraits\\texture.tga"
+        portrait.rareeliteFile = "Interface\\Addons\\Blinkiis_Portraits\\texture.tga"
+        portrait.bossFile = "Interface\\Addons\\Blinkiis_Portraits\\texture.tga"
 
-		portrait:SetScript("OnEvent", UpdatePortrait)
+        for _, event in ipairs(events) do
+            portrait:RegisterEvent(event)
+        end
 
-		if not InCombatLockdown() then
-			portrait:SetSize(64, 64)
-			portrait:ClearAllPoints()
-			portrait:SetPoint("LEFT", portrait.parentFrame, "RIGHT", 0, 0)
-		end
+        portrait:SetScript("OnEvent", UpdatePortrait)
 
-		portrait.texture = portrait:CreateTexture("BP_texture-" .. unitType, "OVERLAY", nil, 4)
-		portrait.texture:SetAllPoints(portrait)
-		portrait.texture:SetTexture("Interface\\Addons\\Blinkiis_Portraits\\texture.tga", "CLAMP", "CLAMP", "TRILINEAR")
+        if not InCombatLockdown() then
+            portrait:SetSize(portrait.size, portrait.size)
+            portrait:ClearAllPoints()
+            portrait:SetPoint(portrait.point.point, portrait.parentFrame, portrait.point.relativePoint, portrait.point.x, portrait.point.y)
+        end
 
-		portrait.portrait = portrait:CreateTexture("BP_portrait-" .. unitType, "OVERLAY", nil, 1)
-		portrait.portrait:SetAllPoints(portrait)
-		SetPortraitTexture(portrait.portrait, portrait.parentFrame.unit, true)
+        -- texture
+        portrait.texture = portrait:CreateTexture("BP_texture-" .. unitType, "OVERLAY", nil, 4)
+        portrait.texture:SetAllPoints(portrait)
+        portrait.texture:SetTexture(portrait.textureFile, "CLAMP", "CLAMP", "TRILINEAR")
 
-		portrait.mask = portrait:CreateMaskTexture()
-		portrait.mask:SetAllPoints(portrait)
-		portrait.mask:SetTexture(
-			"Interface\\Addons\\Blinkiis_Portraits\\mask.tga",
-			"CLAMPTOBLACKADDITIVE",
-			"CLAMPTOBLACKADDITIVE"
-		)
-		portrait.portrait:AddMaskTexture(portrait.mask)
+        -- mask
+        portrait.mask = portrait:CreateMaskTexture()
+        portrait.mask:SetAllPoints(portrait)
+        portrait.mask:SetTexture(portrait.maskFile, "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
 
-		-- scripts to interact with mouse
-		portrait:SetAttribute("unit", portrait.unit)
-		portrait:SetAttribute("*type1", "target")
-		portrait:SetAttribute("*type2", "togglemenu")
-		portrait:SetAttribute("type3", "focus")
-		portrait:SetAttribute("toggleForVehicle", true)
-		portrait:SetAttribute("ping-receiver", true)
-		portrait:RegisterForClicks("AnyUp")
+        -- portrait
+        portrait.portrait = portrait:CreateTexture("BP_portrait-" .. unitType, "OVERLAY", nil, 2)
+        portrait.portrait:SetAllPoints(portrait)
+        portrait.portrait:AddMaskTexture(portrait.mask)
+        SetPortraitTexture(portrait.portrait, portrait.parentFrame.unit, true)
 
-		BPP[unitType] = portrait
-	end
+        -- bg
+        portrait.bg = portrait:CreateTexture("BP_bg-" .. unitType, "OVERLAY", nil, 1)
+        portrait.bg:SetAllPoints(portrait)
+        portrait.bg:AddMaskTexture(portrait.mask)
+        portrait.bg:SetTexture(portrait.bgFile, "CLAMP", "CLAMP", "TRILINEAR")
+        portrait.bg:SetVertexColor(0, 0, 0, 1)
+
+        -- scripts to interact with mouse
+        portrait:SetAttribute("unit", portrait.unit)
+        portrait:SetAttribute("*type1", "target")
+        portrait:SetAttribute("*type2", "togglemenu")
+        portrait:SetAttribute("type3", "focus")
+        portrait:SetAttribute("toggleForVehicle", true)
+        portrait:SetAttribute("ping-receiver", true)
+        portrait:RegisterForClicks("AnyUp")
+
+        BPP[unitType] = portrait
+    end
 end
+
+
+local function UpdateSettings(frame, unit)
+	frame.settings = BLINKIISPORTRAITS.db[unit]
+end
+
 function BLINKIISPORTRAITS:Initialize()
 	local unitframes = nil
 	local events = {
 		player = { "UNIT_PORTRAIT_UPDATE", "PORTRAITS_UPDATED" },
 		target = { "UNIT_PORTRAIT_UPDATE", "PORTRAITS_UPDATED", "PLAYER_TARGET_CHANGED" },
+		focus = { "UNIT_PORTRAIT_UPDATE", "PORTRAITS_UPDATED", "PLAYER_FOCUS_CHANGED", "GROUP_ROSTER_UPDATE" },
+		targettarget = { "UNIT_PORTRAIT_UPDATE", "PORTRAITS_UPDATED", "UNIT_TARGET", "GROUP_ROSTER_UPDATE" },
+		pet = { "UNIT_PORTRAIT_UPDATE", "PORTRAITS_UPDATED", "UNIT_PET", "UNIT_EXITED_VEHICLE", "PET_UI_UPDATE" },
+		party = { "UNIT_PORTRAIT_UPDATE", "PORTRAITS_UPDATED", "UNIT_PET", "UNIT_EXITED_VEHICLE", "PET_UI_UPDATE" },
 	}
 
 	if IsAddOnLoaded("ShadowedUnitFrames") then
 		unitframes = unitframesDB.suf
+	elseif IsAddOnLoaded("ElvUI") then
+		unitframes = unitframesDB.elvui
 	end
 
 	if unitframes then
 		CreatePortrait("player", unitframes, events.player)
 		CreatePortrait("target", unitframes, events.target)
+		CreatePortrait("focus", unitframes, events.focus)
+		CreatePortrait("targettarget", unitframes, events.targettarget)
+		CreatePortrait("pet", unitframes, events.pet)
 	end
 end
