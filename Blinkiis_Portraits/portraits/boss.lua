52c55f32f167ee7cd7276767f9ab6e0d4970eae6
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2025-01-02 22:33:28 +0100

    Update boss.lua

--- Blinkiis_Portraits/portraits/boss.lua
+++ Blinkiis_Portraits/portraits/boss.lua
@@ -24,7 +24,7 @@ local function OnEvent(portrait, event, eventUnit)
 		if color then portrait.texture:SetVertexColor(color.r, color.g, color.b, color.a or 1) end
 
 		BLINKIISPORTRAITS:UpdatePortrait(portrait, event)
-		BLINKIISPORTRAITS:UpdateExtraTexture(portrait, portrait.db.unitcolor and color, portrait.db.extra)
+		BLINKIISPORTRAITS:UpdateExtraTexture(portrait, portrait.db.unitcolor and color)
 
 		portrait.forceUpdate = false
 	else
